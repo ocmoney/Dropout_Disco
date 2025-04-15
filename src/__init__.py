dfd3968c39461ed8a807d870af8674f297af38b0
WORKSPACE <yurii.oksamytnyi@yuriodev.co.uk>
2025-04-15 14:39:09 +0100

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,17 +1,15 @@
 # Hacker News Upvote Prediction
-# Copyright (c) 2025 Dropout Disco Team (Yurii, James, Ollie, Emil)
+# Copyright (c) 2024 Dropout Disco Team (Yurii, James, Ollie, Emil)
 # File: src/__init__.py
-# Description: Initializes the word2vec package.
+# Description: Initializes the src package.
 # Created: 2024-04-15
 # Updated: 2024-04-15
 
+try:
+    from utils import logger
+    logger.debug("SRC package initialized.")
+except ImportError:
+    # Handle case where utils might not be importable during initial scan
+    pass
 
-# Makes the 'word2vec' directory a package.
-
-# You can optionally import key classes/functions here for easier access
-from .model import CBOW
-from .vocabulary import Vocabulary
-from .dataset import create_cbow_dataset
-from .trainer import train_model
-
-__all__ = ['CBOW', 'Vocabulary', 'create_cbow_dataset', 'train_model']
+print("--- Running src/__init__.py ---") # Add this temporarily to confirm it runs
