3b73251beedb2d4fd549597bdc1be46d30099b71
WORKSPACE <yurii.oksamytnyi@yuriodev.co.uk>
2025-04-16 16:28:17 +0100

    Update __init__.py

--- src/word2vec/__init__.py
+++ src/word2vec/__init__.py
@@ -6,12 +6,14 @@
 # Updated: 2025-04-15
 
 # Expose key components for easier importing if desired
-from .model import CBOW
+from .model import CBOW, SkipGram
 from .vocabulary import Vocabulary
 from .dataset import create_cbow_pairs, CBOWDataset
+from .dataset import create_skipgram_pairs, SkipGramDataset
 from .trainer import train_model
 
-__all__ = ['CBOW', 'Vocabulary', 'create_cbow_pairs', 'CBOWDataset', 'train_model']
+__all__ = ['CBOW', 'Vocabulary', 'create_cbow_pairs', 'CBOWDataset', 'train_model', 
+              'train_epoch', 'SkipGram', 'create_skipgram_pairs', 'SkipGramDataset']
 
 # You can add a logger statement here if needed, but it's often kept minimal.
 from utils import logger
