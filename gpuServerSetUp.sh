32f1be018a22ba54e1a0e0b6d6facb44be953a07
James Love Ubuntu Laptop <jimmeylove@gmail.com>
2025-04-17 09:53:09 +0100

    updates

---
+++ gpuServerSetUp.sh
@@ -0,0 +1,11 @@
+/bin/bash
+
+
+git clone https://github.com/ocmoney/Dropout_Disco.git
+git config --global user.name "Your Name"
+git config --global user.email "you@example.com"
+
+
+python -m venv .venv
+source ./.venv/bin/activate
+pip install -r requirementsLinux.txt 
