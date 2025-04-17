7875657fc448714135d524ac2e19fecb9dc3b297
James Love Ubuntu Laptop <jimmeylove@gmail.com>
2025-04-17 10:26:58 +0100

    updates

--- gpuServerSetUp.sh
+++ gpuServerSetUp.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+# -- Instructions-
+# chmod u+x ./gpuServerSetUp.sh 
+# source ./gpuServerSetUp.sh 
+# ----------------
 
 git clone https://github.com/ocmoney/Dropout_Disco.git
 git config --global user.name "GPU Server"
 git config --global user.email "jimmeylove@gmail.com"
 
 cd ./Dropout_Disco
+git checkout feature/model
 python -m venv .venv
 source ./.venv/bin/activate
 pip install -r requirementsLinux.txt 
